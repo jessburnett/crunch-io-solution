e0da068bea601bda7815a6a975f22b397e216600
jessburnett <jess.a.burnett@gmail.com>
2017-06-14 16:58:44 +0000

    add http layer to consume json data|order

--- app/kata/kata.js
+++ app/kata/kata.js
@@ -7,11 +7,22 @@ angular.module('kataSolution.kata', ['ngRoute'])
   });
 }])
 
-.controller('KataCtrl', ['$scope', '$http', function($scope, $http) {
-  console.log($scope); //test scope of Kata Ctrl JB
-  $http.get('fixtures/order.json')
-       .then(function(res){
-          $scope.order = res.data;    
-          console.log('jess');
-        });
+// .controller('KataCtrl', [function($scope, $http) {
+//   console.log($scope); //test scope of Kata Ctrl JB
+//   $http.get('../fixtures/order.json')
+//     .then(function(response) {
+//       $scope.orderData = response.data;
+//   })
+
+.controller('KataCtrl', ['$scope', '$http', function ($scope, $http) {
+    $http.get('fixtures/order.json')
+        .success(function(data) {
+          $scope.orders = data;
+          alert(data.element);
+        })
+        .error(function(data) {
+          // log error
+          alert('error');
+    });
 }]);
+
